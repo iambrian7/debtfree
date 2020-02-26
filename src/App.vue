6efe18a51ba32aa0fe34b5e519767f50ad59664d
bkc <bkcgee@gmail.com>
2020-02-26 11:29:07 -0600

    add login, flexbar and layout grid

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
   <div id="app">
-    <div id="nav">
+     <flex-bar></flex-bar>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/account">Account</router-link> |
       <router-link to="/about">About</router-link>
+    </div> -->
+       <div class="editor">
+       <router-view/>
     </div>
-    <router-view/>
+    <!--Footer-->
+    <div class="footer">
+      <div class="footer-file-info">
+        <span class="file-info-source">src/styles.less</span>
+        <span class="file-info-location">2:5</span>
+        </div>
+      <div class="footer-controls">
+        <a href="">LF</a>
+        <a href="">UTF-8</a>
+        <a href="">Less</a>
+        <a href=""><span class="icon-git-branch"></span>master</a>
+        <a href="">⬇︎ 5 ⬆︎</a>
+        <a href=""><span class="icon-diff"></span>1 file</a>
+        <a href=""><span class="icon-squirrel"></span></a>
+      </div>
+      <!-- end footer -->
+    </div>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import FlexBar from '@/components/FlexBar.vue'
 
+export default {
+  name: 'home',
+  components: {
+    "flex-bar": FlexBar
+  }
+}
+</script>
 <style>
+body {
+      background-color: #d7dae0;
+      font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+      font-size: 0.8rem;
+      color: #2c313a;
+      margin: 0;
+    }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    display: grid;
+    grid-template-rows: 30px auto 30px;
+    grid-template-columns: 300px 1fr;
+    height: 100vh;
 }
-
 #nav {
-  padding: 30px;
+    display: flex;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    justify-content: space-evenly;
 }
+.editor{
+  display: grid;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    height: calc(100vh - 60px);
+    overflow: auto;
+}
+    .footer {
+      display: grid;
+      grid-template-columns: 1fr auto;
+      grid-column-start: 1;
+      grid-column-end: 3;
+      background: #42b983;
+    }
+    .footer-file-info {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .footer-controls {
+      display: flex;
+      height: 100%;
+      justify-content: flex-end;
+    }
+    .footer-controls a {
+      height: 100%;
+      display: flex;
+      align-items: center;
+    }
 
 #nav a {
   font-weight: bold;
--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,16 @@ const routes = [
     }
   },
   {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../components/Login.vue')
+    }
+  },
+  {
     path: '/newaccount',
     name: 'NewAccount',
     // route level code-splitting
